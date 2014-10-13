0c48a08e475b08bdd4a6501a39670b58d833e213
swb1701 <scott_bennett@sra.com>
2014-10-12 21:59:46 -0400

    add calculation of maximum payment per month for each person

--- SimplePayAnalyzer/grails-app/services/com/swblabs/SPAnalyzerService.groovy
+++ SimplePayAnalyzer/grails-app/services/com/swblabs/SPAnalyzerService.groovy
@@ -5,24 +5,69 @@ import grails.transaction.Transactional
 @Transactional
 class SPAnalyzerService {
 	
-	def updateMap(nameMap,trans) {
+	def updateMap(nameMap,trans,oldindex,newindex) {
 	  def rec=nameMap[trans.name]
 	  if (rec==null) {
-		  rec=["total":0,"totalTrans":0,"name":trans.name]
+		  rec=["total":0,"totalTrans":0,"name":trans.name,"months":new Object[newindex-oldindex+1]]
 		  nameMap[trans.name]=rec
 	  }
 	  rec.total+=trans.amount
 	  rec.totalTrans++
+	  def monthIndex=getMonthIndex(trans.date)-oldindex
+	  println(monthIndex)
+	  def oldmonth=rec.months[monthIndex]
+	  if (oldmonth==null) {
+		  oldmonth=["max":0]
+		  rec.months[monthIndex]=oldmonth
+	  }
+	  if (trans.amount>oldmonth.max) {
+		  oldmonth.max=trans.amount
+	  }
 	}
 	
     def analyze() {
+		def oldest=getOldestDate()
+		def newest=getNewestDate()
+		def oldindex=getMonthIndex(oldest)
+		def newindex=getMonthIndex(newest)
+		println("oldest date="+oldest+" newest="+newest+" indexdif="+(newindex-oldindex))
 		def nameMap=[:]
 		SPTransaction.all.each {
-			updateMap(nameMap,it)
+			updateMap(nameMap,it,oldindex,newindex)
 		}
 		println(nameMap)
 		nameMap.values().sort{ a,b -> a.total<=>b.total}.each {
 			println(it.toString()+" avg="+(it.total/it.totalTrans))
 		}
     }
+	
+	def getMonthIndex(Date date) {
+		Calendar cal=Calendar.getInstance()
+		cal.setTime(date)
+		int year=cal.get(Calendar.YEAR)
+		int mon=cal.get(Calendar.MONTH)
+		return(year*12+mon)
+	}
+	
+	def getOldestDate() {
+		def all=SPTransaction.all
+		def result=all.first().date
+		all.each {
+			if (it.date.compareTo(result)<0) {
+				result=it.date
+			}
+		}
+		return(result)
+	}
+	
+	def getNewestDate() {
+		def all=SPTransaction.all
+		def result=all.first().date
+		all.each {
+			if (it.date.compareTo(result)>0) {
+				result=it.date
+			}
+		}
+		return(result)
+	}
 }
